200e54f6b6f7e1310a7793ff5d585c557164a3c5
Michael Iglesias <mike.iglesias11@gmail.com>
2018-07-28 11:58:08 -0700

    fixed typo

--- src/index.js
+++ src/index.js
@@ -103,7 +103,7 @@ export default class MaterialDropZone extends React.Component {
         });
     }
 
-    handleRequestCloseSnackBar = () => {
+    handleRequestCloseSnackBar() {
         this.setState({
             openSnackBar: false,
         });
